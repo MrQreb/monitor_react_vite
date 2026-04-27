7475d23646b0692caf56dd31906a8391e594b833
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2026-04-27 12:24:59 -0600

    trabajando en dark mode

--- src/features/menu/pages/MenuPage.tsx
+++ src/features/menu/pages/MenuPage.tsx
@@ -1,8 +1,10 @@
 import { useMemo, useState } from 'react';
 import NavBar from '@/components/common/NavBar/NavBar';
-import { Carrot, ChartColumnBig, Factory, ThermometerSnowflake } from 'lucide-react';
+import { Carrot, ChartColumnBig, Factory, ThermometerSnowflake, Sun, Moon } from 'lucide-react';
 import { CardMenu } from '../components/CardMenu';
 import { TabsMenu } from '../components/TabsMenu';
+import { Button } from '@/components/ui/button';
+import { useTheme } from '../../../app/providers/ThemeProvider';
 
 
 type Tab = 'Ambas' | 'Planta 1' | 'Planta 3' | 'Cedis';
@@ -44,17 +46,39 @@ export const MenuPage = () => {
     setTab(value as Tab);
   };
 
+  const { setTheme, theme} = useTheme();
+
   return (
 
     <section className='h-screen bg-[#2f2f2f]'>
 
+      <NavBar />
+
 
       <div className="flex w-full h-auto  min-w-0 flex-col bg-[#2f2f2f] px-2 pb-2 pt-1 md:px-3">
-        <NavBar />
 
-        <div className="sticky top-0 mt-4 flex items-center justify-center">
-          <TabsMenu value={tab} onChange={handleTabChange} />
-        </div>
+        <section className="sticky top-0 mt-4 grid grid-col-10">
+
+          <div className='col-start-6 col-end-8'>
+            <TabsMenu value={tab} onChange={handleTabChange} />
+          </div>
+
+
+          <div className="col-start-11">
+            <Button
+              size="icon"
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+              aria-label="Cambiar tema"
+              className="rounded-full text-muted-foreground hover:text-foreground bg-white"
+            >
+              {theme === 'dark' ? <Sun className="h-4 w-4" /> : <Moon className="h-4 w-4" />}
+            </Button>
+          </div>
+
+
+        </section>
+
+
 
         <section className="mt-8 grid gap-12 md:grid-cols-2 xl:grid-cols-3">
           {filteredItems.map((item, index) => (
